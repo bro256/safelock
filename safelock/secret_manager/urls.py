d505e5c0ca25275cac0e54a6639961f717e42515
bro256 <bronius256@gmail.com>
2023-06-29 11:31:59 +0300

    login updated

--- safelock/secret_manager/urls.py
+++ safelock/secret_manager/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    # path('secrets/', views.SecretListView.as_view(), name='secret_list'),
 ]
--- safelock/secret_manager/views.py
+++ safelock/secret_manager/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
 
 def index(request):
     context = {
         'test' : 'test',
     }
     return render(request, 'secret_manager/index.html', context)
+
