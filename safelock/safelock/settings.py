98b557378a2e2a48ce6e8af89f2a45fd8feef152
bro256 <bronius256@gmail.com>
2023-06-29 09:05:12 +0300

    User login settings, urls

--- safelock/safelock/settings.py
+++ safelock/safelock/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'secret_manager',
+    'user_profile'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -123,3 +124,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = '/'
