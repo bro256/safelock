af6fee292c3ba7e539894e8821d6d5569eeb016a
bro256 <bronius256@gmail.com>
2023-06-29 11:33:36 +0300

    requirements updated

--- safelock/secret_manager/views.py
+++ safelock/secret_manager/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.views import generic
 
+from django.contrib.auth import authenticate, login
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives import hashes
+
 def index(request):
     context = {
         'test' : 'test',
