b7b2bbbc2a0447de6bdf086e57f377bc0cd857d3
bro256 <bronius256@gmail.com>
2023-07-07 11:37:20 +0300

    User signup custom form

--- safelock/user_profile/views.py
+++ safelock/user_profile/views.py
@@ -1,12 +1,41 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.contrib import messages
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.decorators.csrf import csrf_protect
 
-from django.contrib.auth import authenticate, login
-from django.contrib.auth.views import LoginView
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-from cryptography.hazmat.primitives import hashes
-from django.shortcuts import redirect, render
+User = get_user_model()
 
-from django.http import HttpResponse
-
-# Create your views here.
-
+# @csrf_protect
+def signup(request):
+    if request.user.is_authenticated:
+        messages.info(request, 'In order to sign up, you need to logout first')
+        return redirect('index')
+    if request.method == "POST":
+        error = False
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        password_confirm = request.POST.get('password_confirm')
+        if not username or len(username) < 5 or User.objects.filter(username=username).exists():
+            error = True
+            messages.error(request, 'Username is too short or already exists.')
+        if not email or len(email) < 5 or User.objects.filter(email=email).exists():
+            error = True
+            messages.error(request, 'Email is invalid or user with this email already exists.')
+        if not password or not password_confirm or password != password_confirm or len(password) < 8:
+            error = True
+            messages.error(request, "Password must be at least 8 characters long and match.")
+        if not error:
+            user = User.objects.create(
+                username=username,
+                email=email,
+                first_name=first_name,
+                last_name=last_name,
+            )
+            user.set_password(password)
+            user.save()
+            messages.success(request, "User registration successful!")
+            return redirect('login')
+    return render(request, 'user_profile/signup.html')
